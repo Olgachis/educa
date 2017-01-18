dc9b04c21731dbb29b98452d29dc98a54439da2f
Eduardo Díaz <iamedu@gmail.com>
2017-01-18 07:25:20 -0600

    Adding educa sql

---
+++ src/main/resources/db/migration/V15__additional_users.sql
@@ -0,0 +1,96 @@
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('54', 'APAC, I.A.P. Asociación Pro-Personas con Parálisis Cerebral', 'Dr. Arce', 'I.A.P.', '1', false, false, false, false, false, true, true, 0, current_timestamp, current_timestamp);
+
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('55', 'Asociación Maka Weiner, I.A.P.', 'Iztapalapa', 'I.A.P.', '2', false, false, false, true, false, false, false, 0, current_timestamp, current_timestamp);
+
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('56', 'Centro de Bienestar y Asistencia Infantil de Jalisco, A.C. ', 'Hogares Complementarios', 'A.C.', '7', false, true, false, false, false, false, false, 0, current_timestamp, current_timestamp);
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('57', 'Centro de Bienestar y Asistencia Infantil de Jalisco, A.C. ', 'El Vergel', 'A.C.', '7', false, false, false, true, false, false, false, 0, current_timestamp, current_timestamp);
+
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('58', 'Ciudad de los Niños de Monterrey, A.B.P.', 'Mujeres', 'A.B.P.', '12', false, false, false, true, true, true, true, 0, current_timestamp, current_timestamp);
+
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('59', 'Escuela Isabel C. de Talamas, A.C.', 'Montesinos', 'A.C.', '22', false, false, false, false, false, true, false, 0, current_timestamp, current_timestamp);
+
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('60', 'Fundación Don Bosco para el Desarrollo del Estudiante Morelense, A.C. ', 'Cuernavaca', 'A.C.', '28', false, false, false, false, false, true, true, 0, current_timestamp, current_timestamp);
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('61', 'Fundación Don Bosco para el Desarrollo del Estudiante Morelense, A.C. ', 'Tilzapotla', 'A.C.', '28', false, false, false, false, false, true, true, 0, current_timestamp, current_timestamp);
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('62', 'Fundación Don Bosco para el Desarrollo del Estudiante Morelense, A.C. ', 'Zapata', 'A.C.', '28', false, false, false, false, false, false, true, 0, current_timestamp, current_timestamp);
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('63', 'Fundación Don Bosco para el Desarrollo del Estudiante Morelense, A.C. ', 'Popotlán', 'A.C.', '28', false, false, false, false, false, false, true, 0, current_timestamp, current_timestamp);
+
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('64', 'Fundación Josefa Vergara y Hernández, I.A.P.', 'Varones', 'I.A.P.', '30', false, true, false, false, false, false, false, 0, current_timestamp, current_timestamp);
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('65', 'Fundación Josefa Vergara y Hernández, I.A.P.', 'Ciegos y débiles visuales', 'I.A.P.', '30', false, true, false, true, true, false, false, 0, current_timestamp, current_timestamp);
+
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('66', 'Fundación para la Asistencia Educativa, I.A.P. ', 'Buenavista', 'I.A.P.', '32', false, false, true, true, false, false, false, 0, current_timestamp, current_timestamp);
+
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('67', 'Instituto de Educación Integral, I.A.P.  San Juan Pablo II', 'Buenavista', 'I.A.P.', '36', false, false, false, true, true, false, false, 0, current_timestamp, current_timestamp);
+
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('68', 'Líderes del Siglo, A.C. ', 'Tlajomulco', 'A.C.', '39', false, false, false, false, false, true, false, 0, current_timestamp, current_timestamp);
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('69', 'Líderes del Siglo, A.C. ', 'Ahualulco', 'A.C.', '39', false, false, false, false, false, true, false, 0, current_timestamp, current_timestamp);
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('70', 'Líderes del Siglo, A.C. ', 'Preparatoria', 'A.C.', '39', false, false, false, false, false, false, true, 0, current_timestamp, current_timestamp);
+
+
+INSERT INTO campus(id, name, campus_name, type, institution_id, primary_campus, internship, initial_education, preschool, basic, secondary, high_school, version, date_created, last_updated)
+VALUES('71', 'Voluntarias Vicentinas de la Santa Cruz del Pedregal, I.A.P. ', 'Santa Teresa', 'I.A.P.', '53', false, false, true, true, false, false, false, 0, current_timestamp, current_timestamp);
+
+
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('54', 'APAC2', '$2a$06$Tdqp2mLDyail5pelXJa.VOY0TnyNFB8j35/1MUgn1FhwhUUmz0Kkq', '54', current_timestamp, current_timestamp, 0, 'institution');
+
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('55', 'MAKA2', '$2a$06$rBFegD5PwsDqcV/9bxS2QeP1KNRHgIQCp9ZRoA.YuM16OpH1Mpd6m', '55', current_timestamp, current_timestamp, 0, 'institution');
+
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('56', 'BIENESTAR2', '$2a$06$q6d4OmMxtJxh4uzFyNyAeu8KK6d7BsTNlOwc4WIhwDQ9CoF1k8OSG', '56', current_timestamp, current_timestamp, 0, 'institution');
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('57', 'BIENESTAR3', '$2a$06$EbEjupxGNDzVg4uNziZvSeoiBWLtkDLCC2Mf7W6wX7FSzby89m8K.', '57', current_timestamp, current_timestamp, 0, 'institution');
+
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('58', 'CDNMONTERREY2', '$2a$06$I9goX7XfI3YSfWx.fqkLo.wpX2wuVqtF2DAjsQdEDTS.RLCGU2IYy', '58', current_timestamp, current_timestamp, 0, 'institution');
+
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('59', 'TALAMAS2', '$2a$06$KIipB4pQ2EQaCxHbOcvhIuwtBSmrseW5.vlK5zEjnyOhD37cdRPnW', '59', current_timestamp, current_timestamp, 0, 'institution');
+
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('60', 'DONBOSCO2', '$2a$06$CeyOICyf06JEBE06IdwrVOTZUg4wPgV8p99g5JYUP9WYvIWX0LJKe', '60', current_timestamp, current_timestamp, 0, 'institution');
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('61', 'DONBOSCO3', '$2a$06$QxfOu.7mFlkRudNeEeX.LOpYZK4XLhdc4vgi3jTYxwdRtbhTqPiWy', '61', current_timestamp, current_timestamp, 0, 'institution');
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('62', 'DONBOSCO4', '$2a$06$Cj9Y6AQOWhOU3qb4TDuHC.oUko4JhrLhIcQZae8TdlkDyq2z/bJra', '62', current_timestamp, current_timestamp, 0, 'institution');
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('63', 'DONBOSCO5', '$2a$06$huh6V3VFTA.NWpUCUZxKhOV/DPykLsQX.V3TGFa.FYfrz0.mCSvwG', '63', current_timestamp, current_timestamp, 0, 'institution');
+
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('64', 'FJOSEFA2', '$2a$06$mj17D9l6sJT8aEeCCPQzpuuV6pFDV7EI9d5qIb.2Sqgu37VyfqHEW', '64', current_timestamp, current_timestamp, 0, 'institution');
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('65', 'FJOSEFA3', '$2a$06$se6R3Ojh6YZgA1QzOt7XB.VGKJZJx4wGDy6luWwm1LJ/7.x392RgK', '65', current_timestamp, current_timestamp, 0, 'institution');
+
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('66', 'FAE2', '$2a$06$OluofWPaHXIl3rZNRkZIjeIWGgYGwOUYfkTBmupr3AwU4hAxqK83m', '66', current_timestamp, current_timestamp, 0, 'institution');
+
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('67', 'JUANPABLO2', '$2a$06$I4KIwJAapFpLMnhdUEeGce40DI2GuGov/obDMvjweOT1IL227.G1W', '67', current_timestamp, current_timestamp, 0, 'institution');
+
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('68', 'LISI2', '$2a$06$Vwa9nvudQZ.NU.udVEcE.Omd7akmPANm/GNTxpan6MRGdqYWmNA1.', '68', current_timestamp, current_timestamp, 0, 'institution');
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('69', 'LISI3', '$2a$06$SDGPCBKFGcw3epjGGJV6aOAsflSPpIaXxEcmAAuPFGLQYb/jxau8y', '69', current_timestamp, current_timestamp, 0, 'institution');
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('70', 'LISI4', '$2a$06$9Rmm3vuS1jWHliKxhx9RtuKfIMN5.ck1N1RQ1nk4SIH8LzmyhEBO.', '70', current_timestamp, current_timestamp, 0, 'institution');
+
+INSERT INTO educa_user (id, username, password, campus_id, date_created, last_updated, version, role_id)
+VALUES ('71', 'VPEDREGAL2', '$2a$06$NYrSUcgJkbPoVAhJUCOOa.FrN6e/mKc4Hkt375LhisBTnceylPgJy', '71', current_timestamp, current_timestamp, 0, 'institution');
+
