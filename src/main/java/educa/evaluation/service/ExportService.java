1c1555545be85969a00e9a6b3d3ad7f1a2ffabe0
Daniel Ramirez <danielramirezhinojosa@gmail.com>
2017-04-06 21:15:29 -0500

    orden de preguntas

--- src/main/java/educa/evaluation/service/ExportService.java
+++ src/main/java/educa/evaluation/service/ExportService.java
@@ -8,24 +8,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.stream.Collectors;
 import java.util.Map;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Slf4j
 @Service
 @Transactional
 public class ExportService {
 
+
+
     @Autowired
     private SimpleQuestionnaireService simpleQuestionnaireService;
 
     public String generateCsv(String id) {
         StringBuilder stringBuilder = new StringBuilder();
-
+        //lista para el orden de las preguntas
+        List<String> questionsOrder = new ArrayList<String>();
         List<SimpleQuestionnaireResponse> responses =
             simpleQuestionnaireService.listFullResponses(id)
                 .getResponses();
 
         if(responses.size() > 0) {
-            stringBuilder.append(buildHeader(responses.get(0).getData()));
+            stringBuilder.append(buildHeader(responses.get(0).getData(), questionsOrder));
         }
 
         responses
@@ -36,15 +41,16 @@ public class ExportService {
                     .append("|")
                     .append(response.getTitle())
                     .append("|")
-                    .append(buildResponses(response.getData()))
+                    .append(buildResponses(response.getData(), questionsOrder))
                     .append("\n");
             });
 
         return stringBuilder.toString();
     }
 
-    private String buildHeader(Map<String, Object> data) {
+    private String buildHeader(Map<String, Object> data, List<String> questionsOrder) {
         StringBuilder header = new StringBuilder();
+
         List<Object> questions = (List<Object>) data.get("questions");
         header
             .append("id")
@@ -56,6 +62,7 @@ public class ExportService {
                 .map(q -> {
                     Map<String, Object> question = (Map<String, Object>) q;
                     String questionData;
+                    questionsOrder.add((String) question.get("displayName"));
                     if(question.get("type").equals("multioptions")){
                       questionData = buildMultivalueQuestion(question);
                     }else{
@@ -81,8 +88,23 @@ public class ExportService {
       .collect(Collectors.joining("|"));
     }
 
-    private String buildResponses(Map<String, Object> data) {
-        List<Object> questions = (List<Object>) data.get("questions");
+    private String buildResponses(Map<String, Object> data, List<String> questionsOrder) {
+      List<Object> questions = (List<Object>) data.get("questions");
+
+      return questionsOrder.stream()
+        .map(q ->{
+        Optional<Object> recoverQuestion = questions
+          .stream()
+          .filter(a -> {
+            Map<String, Object> question = (Map<String, Object>) a;
+            String displayName = (String) question.get("displayName");
+            return displayName.equals(q);
+          })
+          .findFirst();
+          return responseToString(recoverQuestion.get());
+      })
+        .collect(Collectors.joining("|"));
+      /*
         return questions
             .stream()
             .map(q -> {
@@ -98,6 +120,20 @@ public class ExportService {
                 return (String) question.get("value");
             })
             .collect(Collectors.joining("|"));
+            */
+    }
+
+    private String responseToString(Object recoverQuestion){
+      Map<String, Object> question = (Map<String, Object>) recoverQuestion;
+      StringBuilder stringBuilder = new StringBuilder();
+      if(question.get("type").equals("options")){
+        stringBuilder.append(buildOptionResponse(question));
+        return stringBuilder.toString();
+      }
+      if(question.get("type").equals("multioptions") ){
+        return (buildMultivalueResponse(question));
+      }
+      return (String) question.get("value");
     }
 
     private String buildMultivalueResponse(Map<String, Object> question){
