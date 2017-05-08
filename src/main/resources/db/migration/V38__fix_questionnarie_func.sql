d71091b0b927261fae634b5ebc033704a9b1f36d
Daniel Ramirez <danielramirezhinojosa@gmail.com>
2017-05-08 18:20:48 -0500

    Arreglar cuestionarios y get title

---
+++ src/main/resources/db/migration/V38__fix_questionnarie_func.sql
@@ -0,0 +1,5 @@
+UPDATE simple_questionnaire SET title_func = 'response.third_name' WHERE id = 'third_secondEdition';
+UPDATE simple_questionnaire SET title_func = 'response.parents_account_parent_name' WHERE id = 'parents-account_secondEdition';
+UPDATE simple_questionnaire SET title_func = 'response.sixth_name' WHERE id = 'sixth_secondEdition';
+UPDATE simple_questionnaire SET title_func = 'response.teachers_name' WHERE id = 'teachers_secondEdition';
+UPDATE simple_questionnaire SET title_func = 'response.secondary_name' WHERE id = 'secondary_secondEdition';
