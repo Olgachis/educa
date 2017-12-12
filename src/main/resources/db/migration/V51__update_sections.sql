bdb0a23ee2d7ede2b0cf933a1a6f2dc5b18be25f
Daniel Ramirez <danielramirezhinojosa@gmail.com>
2017-12-12 10:29:47 -0600

    Mejoras en autodiagnostico

---
+++ src/main/resources/db/migration/V51__update_sections.sql
@@ -0,0 +1,3 @@
+UPDATE questionnaire_section SET campus_relevant = true
+ WHERE subdimension_id
+ IN ('1.4', '2.5.1', '2.5.2', '2.5.3', '2.5.4', '2.5.5', '2.5.6', '2.5.7', '2.7', '6.1', 	'6.2', 	'7.1', 	'7.2', 	'7.3', 	'7.4', 	'8.1', 	'8.2', 	'8.3', 	'9.1')
