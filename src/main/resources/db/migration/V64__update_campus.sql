506518b2bcb3af622375d488125ca793c76280c0
Daniel Ramirez <danielramirezhinojosa@gmail.com>
2017-12-20 09:27:47 -0600

    actualizar campus

---
+++ src/main/resources/db/migration/V64__update_campus.sql
@@ -0,0 +1,12 @@
+UPDATE campus SET internship = false, initial_education = false, preschool = false, basic = true, secondary = false,
+ high_school = false WHERE id = '42';
+UPDATE campus SET internship = false, initial_education = false, preschool = true, basic = false, secondary = false,
+  high_school = false WHERE id = '48';
+UPDATE campus SET internship = false, initial_education = false, preschool = false, basic = true, secondary = false,
+  high_school = false WHERE id = '14';
+UPDATE campus SET internship = false, initial_education = false, preschool = false, basic = true, secondary = true,
+  high_school = true WHERE id = '12';
+UPDATE campus SET internship = false, initial_education = false, preschool = false, basic = true, secondary = true,
+  high_school = true WHERE id = '12';
+UPDATE campus SET campus_name = 'Mujeres Guadalupe', internship = false, initial_education = false, preschool = false, basic = true, secondary = true,
+  high_school = true WHERE id = '58';
